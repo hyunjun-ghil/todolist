98528ed83a178f3236023594f40070792adc8533
hyunjun-ghil <55752238+hyunjun-ghil@users.noreply.github.com>
2022-03-02 22:38:41 +0900

    1. add data for Today Label   1) calendar click signal added 2. add time for Time Label   2) add timer object

--- main.py
+++ main.py
@@ -1,23 +1,19 @@
-import sys
-import os
+import sys, os, time
 from PyQt5.QtWidgets import *
-from PyQt5 import uic
 from PyQt5.QtGui import *
-from PyQt5 import QtWidgets, QtCore
+from PyQt5.QtCore import *
+from PyQt5 import QtWidgets, QtCore, uic
 from functools import partial
 
-#UI파일 연결
-#단, UI파일은 Python 코드 파일과 같은 디렉토리에 위치해야한다.
-
+# UI파일 연결
+# 단, UI파일은 Python 코드 파일과 같은 디렉토리에 위치해야한다.
 global curItem
-g_FLAG = ""
-LoginId = ""
+
 
 def resource_path(relative_path):
     """ Get absolute path to resource, works for dev and for PyInstaller """
     base_path = getattr(sys, '_MEIPASS', os.path.dirname(os.path.abspath(__file__)))
     return os.path.join(base_path, relative_path)
-
 
 main_class = uic.loadUiType(resource_path("main.ui"))[0]
 
@@ -27,12 +23,23 @@ class MainWindow(QMainWindow, main_class):
         self.setupUi(self)
         self.setWindowTitle("ToDoList")
         self.setWindowIcon(QIcon("titleimage.png"))
+
+        today = QDate.currentDate().toString(Qt.DefaultLocaleLongDate)
+        self.todayLabel.setText(today)
+
+        timer = QTimer(self)
+        timer.timeout.connect(self.showTime)
+        timer.start(200)
+
         self.addListBtn.clicked.connect(self.saveBtnClicked)
+
         self.moveRightBtn.clicked.connect(self.moveRightBtnClicked)
         self.moveLeftBtn.clicked.connect(self.moveLeftBtnClicked)
         self.moveUpBtn.clicked.connect(self.moveUpBtnClicked)
         self.moveDownBtn.clicked.connect(self.moveDownBtnClicked)
+
         self.mainList.itemClicked.connect(self.listClicked)
+        self.calendarWidget.clicked.connect(self.calendarClicked)
 
     def saveBtnClicked(self):
         item = QtWidgets.QListWidgetItem("New List Added:) Write Here")
@@ -64,11 +71,19 @@ class MainWindow(QMainWindow, main_class):
         curItemText = "    " + curItem.text()
         self.mainList.currentItem().setText(curItemText)
 
-
     def moveLeftBtnClicked(self):
         global curItem
         curItemText = curItem.text().replace("    ", "", 1)
         self.mainList.currentItem().setText(curItemText)
+
+    def calendarClicked(self):
+        today = self.calendarWidget.selectedDate().toString(Qt.DefaultLocaleLongDate)
+        self.todayLabel.setText(today)
+
+    def showTime(self):
+        current_time = QTime.currentTime()
+        label_time = current_time.toString('hh:mm:ss')
+        self.timeLabel.setText(label_time)
 
 
 if __name__ == "__main__" :
